02c8d1915d1b985a10cde7ada8c17523f07119bb
vishnumohanan01 <vishnu.mohanan@bigbinary.com>
2022-09-13 23:50:59 +0530

    made Card reusable and moved time funcs to utils

--- app/javascript/src/components/Card.jsx
+++ app/javascript/src/components/Card.jsx
@@ -1,57 +1,55 @@
 import React from "react";
 
 import { MenuVertical, Clock } from "@bigbinary/neeto-icons";
-import moment from "moment";
 import { Tag, Typography, Dropdown, Avatar, Tooltip } from "neetoui";
 
-const Card = ({ title, description, createdAt, dropDownItems }) => {
-  const calculateAgeOfNote = createdAt => moment(new Date(createdAt)).fromNow();
-
-  const formatTime = createdAt => moment(createdAt).format("dd, hh:mm A");
-
-  return (
-    <div className="mb-4 w-full flex-col rounded-sm border border-gray-300 bg-white p-4 shadow-md">
-      <div className="mb-2">
-        <div className="mb-2 flex items-center justify-between">
-          <Typography style="h4">{title}</Typography>
-          {dropDownItems?.length && (
-            <Dropdown
-              buttonStyle="text"
-              className="text-gray-800"
-              icon={() => <MenuVertical color="#68737D" />}
-            >
-              {dropDownItems.map((item, idx) => (
-                <li key={idx} onClick={() => alert(`clicked ${item.name}`)}>
-                  {item.name}
-                </li>
-              ))}
-            </Dropdown>
-          )}
-        </div>
-        <Typography style="body2" weight="light">
-          {description}
-        </Typography>
+const Card = ({
+  title,
+  description,
+  tinyText,
+  toolTipText,
+  avatarUrl,
+  dropDownItems,
+}) => (
+  <div className="mb-4 w-full flex-col rounded-sm border border-gray-300 bg-white p-4 shadow-md">
+    <div className="mb-2">
+      <div className="mb-2 flex items-center justify-between">
+        <Typography style="h4">{title}</Typography>
+        {dropDownItems?.length && (
+          <Dropdown
+            buttonStyle="text"
+            className="text-gray-800"
+            icon={() => <MenuVertical color="#68737D" />}
+          >
+            {dropDownItems.map((item, idx) => (
+              <li key={idx} onClick={() => alert(`Clicked ${item.name}`)}>
+                {item.name}
+              </li>
+            ))}
+          </Dropdown>
+        )}
       </div>
-      <hr />
-      <div className="mt-3 flex justify-between">
-        <Tag color="gray" label="Getting Started" size="small" />
-        <div className="flex flex-row items-center justify-between gap-x-1 p-0">
-          <Clock size={10} />
-          <Tooltip content={formatTime(createdAt)} position="bottom">
-            <Typography style="body3">
-              {`Created ${calculateAgeOfNote(createdAt)}`}
-            </Typography>
-          </Tooltip>
-          <Avatar
-            size="medium"
-            user={{
-              imageUrl: "https://i.pravatar.cc/300",
-            }}
-          />
-        </div>
+      <Typography style="body2" weight="light">
+        {description}
+      </Typography>
+    </div>
+    <hr />
+    <div className="mt-3 flex justify-between">
+      <Tag color="gray" label="Getting Started" size="small" />
+      <div className="flex flex-row items-center justify-between gap-x-1 p-0">
+        <Clock size={10} />
+        <Tooltip content={toolTipText} position="bottom">
+          <Typography style="body3">{tinyText}</Typography>
+        </Tooltip>
+        <Avatar
+          size="medium"
+          user={{
+            imageUrl: avatarUrl,
+          }}
+        />
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default Card;
--- app/javascript/src/components/Dashboard/Notes/ListCards.jsx
+++ app/javascript/src/components/Dashboard/Notes/ListCards.jsx
@@ -3,20 +3,26 @@ import React from "react";
 import Card from "components/Card";
 
 import { NOTES_CARD_DROPDOWN_OPTIONS } from "./constants";
+import { calculateAge, formatTime } from "./utils/prettyPrintTime";
 
-const ListCards = ({ notes = [] }) => (
-  <>
-    {notes.map(notes => (
-      <Card
-        authorID={notes.user_id}
-        createdAt={notes.created_at}
-        description={notes.description}
-        dropDownItems={NOTES_CARD_DROPDOWN_OPTIONS}
-        key={notes.id}
-        title={notes.title}
-      />
-    ))}
-  </>
-);
+const ListCards = ({ notes = [] }) => {
+  const getCreatedAtText = timeStamp => `Created ${calculateAge(timeStamp)}`;
+
+  return (
+    <>
+      {notes.map(note => (
+        <Card
+          avatarUrl={"https://i.pravatar.cc/300"}
+          description={note.description}
+          dropDownItems={NOTES_CARD_DROPDOWN_OPTIONS}
+          key={note.id}
+          tinyText={getCreatedAtText(note.created_at)}
+          title={note.title}
+          toolTipText={formatTime(note.created_at)}
+        />
+      ))}
+    </>
+  );
+};
 
 export default ListCards;
