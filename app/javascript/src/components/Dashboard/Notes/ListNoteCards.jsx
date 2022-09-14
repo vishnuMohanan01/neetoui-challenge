6fcc117544de24f7bacf19437d02509b6bc63b42
vishnumohanan01 <vishnu.mohanan@bigbinary.com>
2022-09-14 14:41:03 +0530

    moved getDropDown components to utils.js

--- app/javascript/src/components/Dashboard/Notes/ListNoteCards.jsx
+++ app/javascript/src/components/Dashboard/Notes/ListNoteCards.jsx
@@ -4,45 +4,31 @@ import PropType from "prop-types";
 
 import Card from "components/Dashboard/Notes/Card";
 
-import { getCreatedAtText, formatTime } from "./utils";
+import { getDropDownItems, getCreatedAtText, formatTime } from "./utils";
 
 const ListNoteCards = ({
   notes = [],
   setShowDeleteAlert,
   setSelectedNoteId,
-}) => {
-  const handleClickOnDelete = note => {
-    setSelectedNoteId(note.id);
-    setShowDeleteAlert(true);
-  };
-
-  const getDropDownItems = note => [
-    {
-      name: "Edit",
-      func: () => {},
-    },
-    {
-      name: "Delete",
-      func: () => handleClickOnDelete(note),
-    },
-  ];
-
-  return (
-    <>
-      {notes.map(note => (
-        <Card
-          avatarUrl={"https://i.pravatar.cc/300"}
-          description={note.description}
-          dropDownItems={getDropDownItems(note)}
-          key={note.id}
-          tinyText={getCreatedAtText(note.created_at)}
-          title={note.title}
-          toolTipText={formatTime(note.created_at)}
-        />
-      ))}
-    </>
-  );
-};
+}) => (
+  <>
+    {notes.map(note => (
+      <Card
+        avatarUrl={"https://i.pravatar.cc/300"}
+        description={note.description}
+        key={note.id}
+        tinyText={getCreatedAtText(note.created_at)}
+        title={note.title}
+        toolTipText={formatTime(note.created_at)}
+        dropDownItems={getDropDownItems(
+          note,
+          setSelectedNoteId,
+          setShowDeleteAlert
+        )}
+      />
+    ))}
+  </>
+);
 
 ListNoteCards.propTypes = {
   notes: PropType.arrayOf(
--- app/javascript/src/components/Dashboard/Notes/utils.js
+++ app/javascript/src/components/Dashboard/Notes/utils.js
@@ -6,3 +6,28 @@ export const formatTime = timeStamp => moment(timeStamp).format("ddd, hh:mmA");
 
 export const getCreatedAtText = timeStamp =>
   `Created ${calculateAge(timeStamp)}`;
+
+export const handleClickOnDelete = (
+  note,
+  setSelectedNoteId,
+  setShowDeleteAlert
+) => {
+  setSelectedNoteId(note.id);
+  setShowDeleteAlert(true);
+};
+
+export const getDropDownItems = (
+  note,
+  setSelectedNoteId,
+  setShowDeleteAlert
+) => [
+  {
+    name: "Edit",
+    func: () => {},
+  },
+  {
+    name: "Delete",
+    func: () =>
+      handleClickOnDelete(note, setSelectedNoteId, setShowDeleteAlert),
+  },
+];
